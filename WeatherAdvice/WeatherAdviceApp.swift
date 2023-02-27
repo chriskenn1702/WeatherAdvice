6db0f3791cc377930877bd15eecec861447e677c
Christopher Kennedy <kennednu@bc.edu>
2023-02-27 17:20:47 -0500

    Initial Commit

---
+++ WeatherAdvice/WeatherAdviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAdviceApp.swift
+//  WeatherAdvice
+//
+//  Created by Christopher Kennedy on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAdviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
